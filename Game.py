3ee45c368a93811614a4ba8917a92c0ebc3773ca
coolwebsite23 <98031434+coolwebsite23@users.noreply.github.com>
2022-01-19 13:17:47 -0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -5,3 +5,7 @@ height = 500
 screen = pygame.display.set_mode(height, width)
 running = True
 while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+ screen.
